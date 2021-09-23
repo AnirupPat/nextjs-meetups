1289712e8eb284d6c133fd60b7c57e70bcd42b0a
Anirup Patnaik <anirup049@gmail.com>
2021-09-24 03:30:43 +0530

    Routing in NextJS

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+    return (
+        <MeetupDetail
+            image="https://images.unsplash.com/photo-1632237794118-36a67cd54ccf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80"
+            title="Second Meetups"
+            address="Duplex No: 242, Pristine Greens, BBSR, Odisha"
+            description="This is out second meetup" />
+
+    )
+}
+
+export default MeetupDetails
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,1 +1,14 @@
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
 // our-domain.com/new-meetup
+
+const NewMeetUpPage = () => {
+    const addMeetupHandler = (enteredMeetupData) => {
+        console.log(enteredMeetupData)
+    }
+    return (
+        <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    )
+}
+
+export default NewMeetUpPage;
